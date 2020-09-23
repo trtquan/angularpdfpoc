6f66ddac943781e37ca79a0298fa5eae439e3d8c
Tran <qtran31@dxc.com>
2020-09-23 11:17:41 +0700

    update CDC component

--- src/app/cdc/cdc.ts
+++ src/app/cdc/cdc.ts
@@ -3,28 +3,28 @@ import {ActivatedRoute} from '@angular/router';
 import {PrintService} from '../print.service';
 
 @Component({
-  selector: 'app-invoice',
-  templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.css']
+  selector: 'app-cdc',
+  templateUrl: './cdc.component.html',
+  styleUrls: ['./cdc.component.css']
 })
-export class InvoiceComponent implements OnInit {
-  invoiceIds: string[];
-  invoiceDetails: Promise<any>[];
+export class CDCComponent implements OnInit {
+  CDCIds: string[];
+  CDCDetails: Promise<any>[];
 
   constructor(route: ActivatedRoute,
               private printService: PrintService) {
-    this.invoiceIds = route.snapshot.params['invoiceIds']
+    this.CDCIds = route.snapshot.params['cdcIds']
       .split(',');
   }
 
   ngOnInit() {
-    this.invoiceDetails = this.invoiceIds
-      .map(id => this.getInvoiceDetails(id));
-    Promise.all(this.invoiceDetails)
+    this.CDCDetails = this.CDCIds
+      .map(id => this.getCDCDetails(id));
+    Promise.all(this.CDCDetails)
       .then(() => this.printService.onDataReady());
   }
 
-  getInvoiceDetails(invoiceId) {
+  getCDCDetails(CDCId) {
     const amount = Math.floor((Math.random() * 100));
     return new Promise(resolve =>
       setTimeout(() => resolve({amount}), 1000)
